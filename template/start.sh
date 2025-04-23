ac086d842e79d9bc7b10c325b819935b7a5ffa67
重劍無鋒 <tecpoirot@foxmail.com>
2025-04-23 17:11:32 +0800

    feat: 优化启动脚本，显示端口号，同时执行测试。

--- template/start.sh
+++ template/start.sh
@@ -7,6 +7,14 @@ readonly BASE_DIR=$THIS_SCRIPT_DIR
 CFG_FILE=$BASE_DIR/conf/ngx.conf
 WORK_DIR=$BASE_DIR/work
 
+if PORT=$(grep listen "$CFG_FILE" | sed -nr 's/[[:space:]]+listen[[:space:]]+([0-9]+);/\1/p'); then
+    echo "listen $PORT;"
+fi
+
+{
+    sleep 1 && curl -I "http://127.0.0.1:${PORT}"
+} &
+
 if command -v tspin >/dev/null; then
     openresty -p "$WORK_DIR" -c "$CFG_FILE" -g 'daemon off;' 2>&1 | tspin
 else
